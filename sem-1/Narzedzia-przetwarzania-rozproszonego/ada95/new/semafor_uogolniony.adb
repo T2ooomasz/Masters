0e6f8b1e5e5af29f4f251e63300426bd377d0c0e
Tomasz Kazulak <tomkazulak@gmail.com>
2025-09-09 13:43:30 +0200

    NPR: ada95 counting semaphore update v4

--- sem-1/Narzedzia-przetwarzania-rozproszonego/ada95/new/semafor_uogolniony.adb
+++ sem-1/Narzedzia-przetwarzania-rozproszonego/ada95/new/semafor_uogolniony.adb
@@ -6,10 +6,19 @@ package body Semafor_Uogolniony is
    
    protected body Counting_Semaphore is
       
-      entry P (Ile : Positive) when Count >= Ile is
+      entry P (Ile : Positive) when True is
+      begin
+         if Count >= Ile then
+            Count := Count - Ile;
+         else
+            requeue Wait with abort;  -- keep parameter Ile!
+         end if;
+      end P;
+      
+      entry Wait (Ile : Positive) when Count >= Ile is
       begin
          Count := Count - Ile;
-      end P;
+      end Wait;
       
       procedure V (Ile : Positive) is
       begin
--- sem-1/Narzedzia-przetwarzania-rozproszonego/ada95/new/semafor_uogolniony.ads
+++ sem-1/Narzedzia-przetwarzania-rozproszonego/ada95/new/semafor_uogolniony.ads
@@ -1,7 +1,7 @@
 package Semafor_Uogolniony is
    
    protected type Counting_Semaphore (Initial_Count : Natural) is
-      -- Acquire Ile resources (block until available)
+      -- Acquire Ile resources (may block until available)
       entry P (Ile : Positive);
       
       -- Release Ile resources
@@ -11,6 +11,7 @@ package Semafor_Uogolniony is
       function Current_Value return Natural;
       
    private
+      entry Wait (Ile : Positive);  -- private requeue target
       Count : Natural := Initial_Count;
    end Counting_Semaphore;
 
